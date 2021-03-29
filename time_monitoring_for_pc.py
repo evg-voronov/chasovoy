f2b3f5783590d5800cee48a391cb6e7542c062f0
Evgeniy Voronov <egn.voronov@gmail.com>
2021-03-29 16:02:22 +0500

    Logging in Excel document. Creating the Excel class

--- time_monitoring_for_pc.py
+++ time_monitoring_for_pc.py
@@ -1,6 +1,67 @@
 import time
 import cv2
+import openpyxl
 from mtcnn.mtcnn import MTCNN
+
+
+class Excel:
+    def __init__(self, today):
+        self.today = today
+        self.wb = openpyxl.load_workbook('log.xlsx')
+        if today not in self.wb.sheetnames:
+            self.new_list()
+        else:
+            self.extract_info()
+
+    def new_list(self):
+        self.wb.create_sheet(title=self.today)
+        self.sheet = self.wb[self.today]
+        self.sheet.column_dimensions['A'].width = 20
+        self.sheet.column_dimensions['B'].width = 20
+        self.sheet.column_dimensions['C'].width = 20
+        self.sheet.column_dimensions['D'].width = 31
+        self.sheet['A1'] = 'Начал работать за ПК'
+        self.sheet['B1'] = 'Время работы за ПК'
+        self.sheet['C1'] = 'Закончил работать'
+        self.sheet['D1'] = 'Сел за компьютер:'
+        self.sheet['D2'] = 'Всего провели за ПК времени:'
+        self.row = 2
+        self.all_time = 0
+
+    def extract_info(self):
+        self.sheet = self.wb[self.today]
+        row = self.sheet['E1'].value  # номер строки в excel документе
+        all_time = self.sheet['E2'].value  # общее время проведенное за ПК за один день
+        all_time = all_time.split(':')
+        self.row = row + 2  # плюсуем шапку таблицы и новую строку
+        self.all_time = int(all_time[0]) * 3600 + int(all_time[1]) * 60 + int(all_time[2])  # время переводим в секунды
+
+    def record(self, start_work, time_break):
+        self.sheet['A' + str(self.row)] = time.strftime("%H:%M:%S",
+                                                        time.localtime(start_work))  # записываем время когда сели за пк
+        self.sheet['C' + str(self.row)] = time.strftime("%H:%M:%S", time.localtime(
+            time.time() - time_break))  # когда вышли из за пк
+        seconds = time.time() - start_work - time_break  # время которое мы провели за компьютером
+        print(time.time(), start_work, time_break)
+        self.sheet['B' + str(self.row)] = time.strftime('%H:%M:%S', time.gmtime(seconds))
+        self.sheet['E1'] = self.row - 1  # вычитаем шапку таблицу
+        all_time = self.all_time + seconds
+        all_time = time.strftime('%H:%M:%S', time.gmtime(all_time))
+        self.sheet['E2'] = all_time
+        # заполняем первый лист
+        sheet_0 = self.wb.worksheets[0]
+        sheet_0['A' + str(len(self.wb.sheetnames))] = self.today
+        sheet_0['B' + str(len(self.wb.sheetnames))] = self.all_time
+
+    def __del__(self):
+        open = True
+        while open:  # закрываем файл
+            try:
+                self.wb.save('log.xlsx')
+                open = False
+            except PermissionError:
+                input('Сначала закройте excel файл и подтвердите нажав enter')
+
 
 class SessionWork:
     def __init__(self):
@@ -10,13 +71,13 @@ class SessionWork:
         self.time_work = 0
 
     def sit(self):
-        if I.start_work == 0:
+        if self.start_work == 0:
             self.start_work = time.time()
         self.start_break = 0
         self.time_break = 0
 
     def stand_up(self):
-        if self.start_break == 0 and I.start_work != 0:
+        if self.start_break == 0 and self.start_work != 0:
             self.start_break = time.time()
 
     def record(self):
@@ -40,12 +101,13 @@ class SessionWork:
         cv2.imshow("I", img)
 
 
-
 cap = cv2.VideoCapture(0)
 detector = MTCNN()
-min_time_work = 1800  # seconds. The minimum time after which the recording in Excel
-min_time_break = 300  # seconds. The minimum break time after which the recording in Excel
+min_time_work = 4  # 1800  # seconds. The minimum time after which the recording in Excel
+min_time_break = 2  # 300  # seconds. The minimum break time after which the recording in Excel
 I = SessionWork()
+today = time.strftime("%d.%m.%Y", time.localtime(time.time()))
+excel_sheet = Excel(today)
 
 while cap.isOpened():
     _, img = cap.read()
@@ -62,7 +124,9 @@ while cap.isOpened():
     print(f"time_break {I.time_break}")
     print("-------------------")
     if I.time_work > min_time_work and I.time_break > min_time_break:
-        I.record()
+        excel_sheet.record(I.start_work, I.time_break)
+        print("record")
+        del excel_sheet
     I.show_faces(img, faces)
     cv2.waitKey(800)
 
